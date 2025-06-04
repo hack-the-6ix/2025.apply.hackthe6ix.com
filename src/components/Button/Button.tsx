5bf6df38cff4034f412e27a98c184fc29b857e5d
jeffrey-zang <jeffrey.zang.07@gmail.com>
2025-06-04 07:47:16 -0400

    update button style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -15,23 +15,41 @@ const Button: React.FC<ButtonProps> = ({
   darkMode = false,
   ...props
 }) => {
-  const baseStyles = `disabled:bg-gray-400 disabled:text-gray-100 disabled:cursor-not-allowed disabled:border-0 rounded-md px-4 py-2 font-semibold transition sm:w-[110px] h-[44px] w-full border flex items-center justify-center`;
+  const baseStyles = `disabled:bg-gray-400 disabled:text-gray-100 disabled:cursor-not-allowed disabled:border-0 rounded-md px-4 py-2 font-semibold transition sm:w-[110px] h-[44px] w-full border flex items-center justify-center cursor-pointer`;
   const variantStyles =
     variant === "back"
-      ? `bg-transparent text-[#00887E] hover:bg-[#00887E] hover:border-transparent hover:text-white  ${darkMode ? "border-white text-white" : "border-[#00887E]"}`
-      : `bg-[#00887E] border-transparent text-white hover:bg-transparent ${darkMode ? "text-white hover:text-white hover:border-white" : "hover:border-[#00887E] hover:text-[#00887E]"}`;
+      ? `bg-transparent text-[#00887E] hover:bg-[#a0e6fa]  ${
+          darkMode ? "border-white text-white" : "border-[#00887E]"
+        }`
+      : `bg-[#00887E] border-transparent text-white hover:bg-[#007a71] ${
+          darkMode ? "text-white hover:border-white" : "hover:border-[#00887E]"
+        }`;
 
-      if (children){
-        return (
-            <button className={`${baseStyles} ${variantStyles} ${className}`} {...props}>
-              {children}
-            </button>
-          );
-      }
+  if (children) {
+    return (
+      <button
+        className={`${baseStyles} ${variantStyles} ${className}`}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
 
   return (
-    <button className={`${baseStyles} ${variantStyles} ${className}`} {...props}>
-      {variant == "next" ? <span className="flex flex-row gap-3 items-center justify-center"><FaArrowRight className="text-[12px]"/> Next</span> : <span className="flex flex-row gap-3 items-center justify-center"><FaArrowLeft className="text-[12px]"/> Back</span>}
+    <button
+      className={`${baseStyles} ${variantStyles} ${className}`}
+      {...props}
+    >
+      {variant == "next" ? (
+        <span className="flex flex-row gap-3 items-center justify-center">
+          <FaArrowRight className="text-[12px]" /> Next
+        </span>
+      ) : (
+        <span className="flex flex-row gap-3 items-center justify-center">
+          <FaArrowLeft className="text-[12px]" /> Back
+        </span>
+      )}
     </button>
   );
 };
